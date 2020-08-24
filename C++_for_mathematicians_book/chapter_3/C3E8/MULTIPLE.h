66d7a6dc90d1f52cb758f81c0185440e5a40ae6f
Rodrigo <rodrigo@emp-235-236.eduroam.uu.se>
2020-08-24 12:47:37 +0200

    more exercises - chapter 3

---
+++ C++_for_mathematicians_book/chapter_3/C3E8/MULTIPLE.h
@@ -0,0 +1,17 @@
+#ifndef MULTIPLE_H
+#define MULTIPLE_H
+
+/*
+* funtion to find the least multiple of n such that
+* it is described by zero and one on basis 10
+*/
+
+
+/*
+*@param n long long number
+*@return leat multiple of n such that on besis 10 is described by 0 and 1
+*/
+
+long long multiple_n(long n);
+
+#endif
