ef1b9fb8aa4a2740648a2c4773de3b411de8eee2
Rodrigo Goncalves Schaefer <rodgo373@tl-tres.math.uu.se>
2020-09-11 10:48:05 +0200

    last exercises of chapter 5

---
+++ C++_for_mathematicians_book/chapter_5_Arrays/C5E10/fibs.h
@@ -0,0 +1,17 @@
+#ifndef FIBS_H
+#define FIBS_H
+
+/*
+FUNTION THAT RETURNS THE NTH- FIBONACCI NUMBER, WITH N BETWEEN 0 AND 40
+
+@PARAM n - the position of the number desired
+
+@return the n-th fibonacci number
+
+*/
+
+
+long fibs( long n);
+
+#endif
+
---
+++ C++_for_mathematicians_book/chapter_5_Arrays/C5E8/max_value.h
@@ -0,0 +1,17 @@
+#ifndef MAX_VALUE_H
+#define MAX_VALUE_H
+
+/*
+FUNCTION THAT returns the maximum values of an array
+
+@param number_elements - number of elements of the array
+@param list - array of long elements
+
+@return - the element with the maximum value 	
+*/
+
+
+long max_value( long number_elements , long* list);
+
+
+#endif
